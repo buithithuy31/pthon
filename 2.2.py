98a47dc256d86d052dbba42aac31c2d7a0580a0c
buithithuy31 <85718515+buithithuy31@users.noreply.github.com>
2021-06-11 10:00:13 +0700

    nguyenhoang092001@gmail.com

---
+++ 2.2.py
@@ -0,0 +1,3 @@
+a=int(input("Nhap Gia niem yet: "))
+b=int(input("Nhap Chiet khau: "))
+print("Gia ban: ",(a-b)*1.01)
---
+++ mau.py
@@ -0,0 +1,5 @@
+print("Hello Worlld!!!")
+a=5
+b=10
+print(a+b)
+print("a+b=",+a+b)
