ec94031487bee197cdec3fdf307e545c948ee9de
buithithuy31 <0703thuy@gmail.com>
2022-04-18 10:07:25 +0700

    Create variables_start.py

---
+++ Python/variables_start.py
@@ -0,0 +1,29 @@
+from operator import truediv
+
+
+myint = 5
+myfloat = 13.2
+mystr = "this is a string"
+mybool = True
+mylist = [0, 1, "two", 3.2, False]
+mytuple = (0, 1, 2)
+mydict = {"one" : 1, "two" :2}
+
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
+
+# myint ="abc"
+# print(myint)
+
+# print(mylist[2])
+# print(mytuple[1])
+
+# print(mylist[1:5])
+# print(mylist[1:5:2])
+
+# print(mylist[::-1])
